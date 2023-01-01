31bf3f04475998ab402723a635fb648913b0ef8b
dmcge <85393840+dmcge@users.noreply.github.com>
2023-01-01 12:36:56 +0000

    Assert that single- and double-quoted strings are the same

--- test/string_test.rb
+++ test/string_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class StringTest < Javascript::Test
   def test_adding_strings_together
     assert_equal "Helloworld", evaluate(%("Hello" + "world"))
-    assert_equal "Hello world", evaluate(%("Hello" + " " + "world"))
+    assert_equal "Hello world", evaluate(%("Hello" + ' ' + "world"))
   end
 
   def test_adding_strings_with_numbers
