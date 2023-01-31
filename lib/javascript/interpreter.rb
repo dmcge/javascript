e925c1c3c0fb2564b9eea9aba8ea8cda7eac704b
dmcge <85393840+dmcge@users.noreply.github.com>
2023-01-31 21:57:57 +0000

    Less of a mouthful

--- lib/javascript/interpreter.rb
+++ lib/javascript/interpreter.rb
@@ -36,7 +36,7 @@ module Javascript
       end
 
       def execute_if_statement(if_statement)
-        if evaluate_expression_to_value(if_statement.condition).truthy?
+        if evaluate_value(if_statement.condition).truthy?
           execute_statement(if_statement.consequent)
         elsif if_statement.alternative
           execute_statement(if_statement.alternative)
@@ -48,15 +48,15 @@ module Javascript
       end
 
       def execute_return_statement(statement)
-        throw :return, evaluate_expression_to_value(statement.expression)
+        throw :return, evaluate_value(statement.expression)
       end
 
       def execute_expression_statement(statement)
-        evaluate_expression_to_value(statement.expression)
+        evaluate_value(statement.expression)
       end
 
 
-      def evaluate_expression_to_value(expression)
+      def evaluate_value(expression)
         evaluate_expression(expression).then do |result|
           if result.is_a?(Reference)
             result.value
@@ -89,11 +89,11 @@ module Javascript
       end
 
       def evaluate_function_call(function_call)
-        function = evaluate_expression_to_value(function_call.callee)
+        function = evaluate_value(function_call.callee)
         previous_identifiers = @references.dup
 
         arguments = function.parameters.zip(function_call.arguments).map do |parameter, argument|
-          @references[parameter] = make_reference(evaluate_expression_to_value(argument)) if argument
+          @references[parameter] = make_reference(evaluate_value(argument)) if argument
         end
 
         catch :return do
@@ -110,7 +110,7 @@ module Javascript
 
       def evaluate_assignment(assignment)
         if @references.include?(assignment.identifier.name)
-          @references[assignment.identifier.name].value = evaluate_expression_to_value(assignment.value)
+          @references[assignment.identifier.name].value = evaluate_value(assignment.value)
         else
           raise "Trying to assign variable #{assignment.identifier.name}, but it doesn’t exist"
         end
@@ -131,20 +131,20 @@ module Javascript
       def evaluate_object_literal(literal)
         Object.new.tap do |object|
           literal.properties.each do |property|
-            object[property.name] = evaluate_expression_to_value(property.value)
+            object[property.name] = evaluate_value(property.value)
           end
         end
       end
 
       def evaluate_array_literal(literal)
         Array.new.tap do |array|
-          literal.elements.each { |element| array << evaluate_expression_to_value(element) }
+          literal.elements.each { |element| array << evaluate_value(element) }
         end
       end
 
       def evaluate_property_access(property_access)
-        receiver = evaluate_expression_to_value(property_access.receiver)
-        accessor = property_access.computed ? property_access.accessor : evaluate_expression_to_value(property_access.accessor)
+        receiver = evaluate_value(property_access.receiver)
+        accessor = property_access.computed ? property_access.accessor : evaluate_value(property_access.accessor)
 
         receiver[accessor]
       end
@@ -154,34 +154,34 @@ module Javascript
         when Operator::Increment, Operator::Decrement
           operation.operator.perform_unary(evaluate_expression(operation.operand), position: operation.position)
         else
-          operation.operator.perform_unary(evaluate_expression_to_value(operation.operand))
+          operation.operator.perform_unary(evaluate_value(operation.operand))
         end
       end
 
       def evaluate_binary_operation(operation)
         case operation.operator
         when Operator::And
-          if (left_hand_side = evaluate_expression_to_value(operation.left_hand_side)).truthy?
-            evaluate_expression_to_value(operation.right_hand_side)
+          if (left_hand_side = evaluate_value(operation.left_hand_side)).truthy?
+            evaluate_value(operation.right_hand_side)
           else
             left_hand_side
           end
         when Operator::Or
-          if (left_hand_side = evaluate_expression_to_value(operation.left_hand_side)).truthy?
+          if (left_hand_side = evaluate_value(operation.left_hand_side)).truthy?
             left_hand_side
           else
-            evaluate_expression_to_value(operation.right_hand_side)
+            evaluate_value(operation.right_hand_side)
           end
         else
-          left_hand_side  = evaluate_expression_to_value(operation.left_hand_side)
-          right_hand_side = evaluate_expression_to_value(operation.right_hand_side)
+          left_hand_side  = evaluate_value(operation.left_hand_side)
+          right_hand_side = evaluate_value(operation.right_hand_side)
 
           operation.operator.perform_binary(left_hand_side, right_hand_side)
         end
       end
 
       def evaluate_parenthetical(parenthetical)
-        evaluate_expression_to_value(parenthetical.expression)
+        evaluate_value(parenthetical.expression)
       end
 
 
